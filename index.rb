e578074752c55928fe8474eb2d6a2f0b5e13aabf
Yonashenok <yonashenok24@gmail.com>
2023-08-30 09:39:39 +0300

    fix: linter error

--- index.rb
+++ index.rb
@@ -1,35 +1,49 @@
+# frozen_string_literal: true
+
 MORSE_CODE = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-    '--..' => 'Z','   ' => ' '
-  }.freeze
+
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+
+  '--..' => 'Z', '   ' => ' '
+
+}.freeze
 
 def decode_char(str)
   MORSE_CODE[str]
 end
 
-puts decode_char(".-")
+puts decode_char('.-')
 
 def decode_word(str)
   decode_str = []
-	str.split(' ').each do |item|   
-    decode_str.push(decode_char(item)) 
+
+  str.split(' ').each do |item|
+    decode_str.push(decode_char(item))
   end
-    decode_str.join
+
+  decode_str.join
 end
 
-puts decode_word("-- -.--")
+puts decode_word('-- -.--')
 
-def decode_massage (str)
+def decode_massage(str)
   decode_str_massage = []
+
   str.split('   ').each do |word|
     decode_str_massage.push(decode_word(word))
   end
-  decode_str_massage.join(' ')
-end 
 
-puts decode_massage("-- -.--   -. .- -- .")
-puts decode_massage(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+  decode_str_massage.join(' ')
+end
+
+puts decode_massage('-- -.--   -. .- -- .')
+
+puts decode_massage('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
